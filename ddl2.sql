047f8d25a4a8d9019c0f35fb24cca0577d35e33d
Abhinn Sarkar <abhinnsarkar@gmail.com>
2024-04-14 16:04:03 -0700

    db processes

--- ddl2.sql
+++ ddl2.sql
@@ -20,6 +20,7 @@ CREATE TABLE public.route (
 CREATE TABLE public.model_seat (
     model_seat_id UUID PRIMARY KEY,
     seat_number VARCHAR(255) NOT NULL,
+    seat_class VARCHAR(255) NOT NULL,
     flight_model_name_key VARCHAR(255) NOT NULL,
     FOREIGN KEY (flight_model_name_key) REFERENCES public.flight_model(flight_model_name_key)
 );
@@ -38,32 +39,26 @@ CREATE TABLE public.flight_schedule (
     flight_schedule_id UUID PRIMARY KEY,
     flight_id UUID NOT NULL,
     departure_date DATE NOT NULL,
-    departure_time TIMESTAMP NOT NULL,
+    departure_time TIME NOT NULL,
     CONSTRAINT unique_flight_schedule UNIQUE (flight_id, departure_date, departure_time)
 );
 
 CREATE TABLE public.seat_allocation (
     seat_allocation_id UUID PRIMARY KEY,
     flight_schedule_id UUID NOT NULL,
-    -- flight_id UUID NOT NULL,
-    -- departure_date DATE NOT NULL,
-    -- departure_time TIMESTAMP NOT NULL,
     model_seat_id UUID NOT NULL,
     available BOOLEAN NOT NULL default true,
-    -- FOREIGN KEY (flight_id) REFERENCES public.flight(flight_id),
     FOREIGN KEY (flight_schedule_id) REFERENCES public.flight_schedule(flight_schedule_id),
-    FOREIGN KEY (model_seat_id) REFERENCES public.model_seat(model_seat_id),
-    -- FOREIGN KEY (flight_id, departure_date, departure_time) REFERENCES public.flight_schedule(flight_id, departure_date, departure_time)
+    FOREIGN KEY (model_seat_id) REFERENCES public.model_seat(model_seat_id)
 );
 
 CREATE TABLE public.ticket (
     ticket_id UUID PRIMARY KEY,
     customer_id UUID NOT NULL,
-    -- flight_id UUID NOT NULL,  the flight id will come from the schdule id
+    -- flight_id UUID NOT NULL,  the flight id will come from the schedule id
     flight_schedule_id UUID NOT NULL,
     seat_allocation_id UUID NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES public.customer(customer_id),
-    -- FOREIGN KEY (flight_id) REFERENCES public.flight(flight_id),
     FOREIGN KEY (flight_schedule_id) REFERENCES public.flight_schedule(flight_schedule_id),
     FOREIGN KEY (seat_allocation_id) REFERENCES public.seat_allocation(seat_allocation_id)
 );
